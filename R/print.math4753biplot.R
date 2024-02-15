e2f11d507f6b09ea769b3d555f0411fd1d99fca8
Brandan Rosa <brandanrosa22@gmail.com>
2024-02-15 14:22:36 -0600

    6th

--- R/print.math4753biplot.R
+++ R/print.math4753biplot.R
@@ -5,14 +5,17 @@
 #' @param x a numeric vector
 #' @param ... passes extra options into the function
 #'
-#' @return a histogram
 #' @importFrom ggplot2 ggplot aes geom_histogram labs
 #' @importFrom rlang .data
 #' @importFrom dplyr %>%
 #'
+#' @return a histogram
+#' @export print.math4753biplot
+#'
+#' @export
+#'
 #' @examples
 #' \dontrun{print.math4753biplot(x = mpg)}
-#' @export
 print.math4753biplot <- function(x, ...){
   inout <- NULL
 
--- R/summary.math4753biplot.R
+++ R/summary.math4753biplot.R
@@ -5,23 +5,26 @@
 #' @param ... additional options
 #' @param object a numeric vector
 #'
-#' @return a summary of mean, median, IQR, n grouped by their .data$inout status
 #' @importFrom dplyr n group_by summarize %>%
 #' @importFrom stats median IQR
 #' @importFrom rlang .data
 #'
+#' @return a summary of mean, median, IQR, n grouped by their .data$inout status
+#' @export summary.math4753biplot
+#'
+#' @export
+#'
 #' @examples
 #' \dontrun{summarymath4753biplot(x = mpg)}
-#' @export
 summary.math4753biplot <- function(object, ...){
   x <- NULL
   inout <- NULL
 
-  df <- data.frame(x = x$x, inout = x$y)
+  df <- data.frame(x = x$x, inout = x$y, ...)
 
   s <- df %>%
-    group_by(.data[[inout]]) %>%
-    summarize(mean = mean(.data[[x]]), median = median(.data[[x]]), IQR = IQR(.data[[x]]), n = n())
+    group_by(inout) %>%
+    summarize(mean = mean(x), median = median(x), IQR = IQR(x), n = n())
 
   print(s)
 }
